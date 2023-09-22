7edd063da000d05afc332a11d883d95cc32cb364
fadi alhabib <fadi.b.alhabib@gmail.com>
2023-09-22 16:00:13 +0300

    pusher fix

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -39,21 +39,24 @@ export async function POST(req: Request) {
     const friend: User = JSON.parse(friendRaw);
 
     await Promise.all([
-      pusherServer.trigger(
+      await pusherServer.trigger(
         toPusherKey(`user:${idToAdd}:friends`),
         'new_friend',
         user
       ),
 
-      pusherServer.trigger(
+      await pusherServer.trigger(
         toPusherKey(`user:${session.user.id}:friends`),
         'new_friend',
         friend
       ),
 
-      db.sadd(`user:${session.user.id}:friends`, idToAdd),
-      db.sadd(`user:${idToAdd}:friends`, session.user.id),
-      db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd),
+      await db.sadd(`user:${session.user.id}:friends`, idToAdd),
+      await db.sadd(`user:${idToAdd}:friends`, session.user.id),
+      await db.srem(
+        `user:${session.user.id}:incoming_friend_requests`,
+        idToAdd
+      ),
     ]);
 
     return new Response('OK');
